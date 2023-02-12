62cb0061558abb3f58b41e7afbcae3db6ea468a0
BIKASH <124684607+Pr0fess0r99@users.noreply.github.com>
2023-02-12 10:16:08 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-echo "Cloning Repo, Please Wait..."
-git clone -b main https://github.com/Pr0fess0r99/LuciferFilter-RoboT.git /LuciferFilter-RoboT
-cd /LuciferFilter-RoboT
-echo "Installing Requirements..."
-pip3 install -U -r requirements.txt
-echo "Starting Bot, Please Wait..."
-python3 bot.py
