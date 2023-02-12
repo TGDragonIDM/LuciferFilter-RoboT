dc294d073df1b9ef1ad24b16c5eccd1b848d7c52
BIKASH <124684607+Pr0fess0r99@users.noreply.github.com>
2023-02-12 20:58:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,12 +14,12 @@ def is_enabled(value, default):
 
 
 # Bot information
-API_ID = environ.get("API_ID", "")
+API_ID = int(environ.get("API_ID", ""))
 API_HASH = environ.get("API_HASH", "")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 # Bot setting
-PICS = (environ.get('PICS', 'https://telegra.ph/file/8b42f6caf6ef5fd76766f.jpg https://telegra.ph/file/82b5bbbab6d5e5593b6b2.jpg'))
+PICS = (environ.get("PICS", ""))
 
 # Admins, Channels & Users
 ADMINS = environ.get("ADMINS", "")
@@ -27,9 +27,10 @@ SUPPORT_CHAT = environ.get("SUPPORT_CHAT", "TechProjectsChats")
 AUTH_CHANNEL = environ.get("AUTH_CHANNEL", "")
 CHANNELS = [int(ch) if search.search(ch) else ch for ch in environ.get("CHANNELS", "0").split()]
 LOG_CHANNEL = environ.get("LOG_CHANNEL", "")
+DELETE_CHANNELS = [int(dch) if search.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 AUTH_GROUPS = environ.get("AUTH_GROUPS", "")
 AUTH_USERS = environ.get("AUTH_USERS", "")
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "tgmoviebot")
+DATABASE_NAME = environ.get('DATABASE_NAME', "")
