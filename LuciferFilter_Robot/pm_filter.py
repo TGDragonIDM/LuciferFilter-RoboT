9696278530f88f80b52206bd3579f6dcb519c3b4
BIKASH <124684607+Pr0fess0r99@users.noreply.github.com>
2023-02-14 10:42:06 +0530

    Update pm_filter.py

--- LuciferFilter_Robot/pm_filter.py
+++ LuciferFilter_Robot/pm_filter.py
@@ -584,7 +584,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text=Script.HELP_TXT.format(query.from_user.mention),
+            text=Script.HELP_TXT.format(query.from_user.mention, name=temp.Bot_Name),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
