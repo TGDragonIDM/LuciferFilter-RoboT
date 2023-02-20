4191829d95732e4c1607258afaf3e3085205d658
BIKASH <124684607+Pr0fess0r99@users.noreply.github.com>
2023-02-20 10:50:04 +0530

    Update index.py

--- LuciferFilter_Robot/index.py
+++ LuciferFilter_Robot/index.py
@@ -3,9 +3,9 @@ import asyncio
 from pyrogram import Client, filters, enums
 from pyrogram.errors import FloodWait
 from pyrogram.errors.exceptions.bad_request_400 import ChannelInvalid, ChatAdminRequired, UsernameInvalid, UsernameNotModified
-from info import ADMINS
+from LuciferFilter_Robot import ADMINS
 from info import INDEX_REQ_CHANNEL as LOG_CHANNEL
-from database.ia_filterdb import save_file
+from LuciferFilter_Robot.ia_filterdb import save_file
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from utils import temp
 import re
