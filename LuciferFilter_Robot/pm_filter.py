6cbf22f2b21f2bf209e2b6ed3c2f580ff2f47673
BIKASH <124684607+Pr0fess0r99@users.noreply.github.com>
2023-02-22 13:03:09 +0530

    Update pm_filter.py

--- LuciferFilter_Robot/pm_filter.py
+++ LuciferFilter_Robot/pm_filter.py
@@ -7,8 +7,7 @@ import pyrogram
 from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
 from LuciferFilter_Robot.translation import Script
 from database.connections_mdb import active_connection, all_connections, delete_connection, if_active, make_active, make_inactive
-from LuciferFilter_Robot import ADMINS, AUTH_CHANNEL, LOG_CHANNEL, AUTH_USERS, CUSTOM_FILE_CAPTION, P_TTI_SHOW_OFF, IMDB, SINGLE_BUTTON, SPELL_CHECK_REPLY, IMDB_TEMPLATE
-from config import AUTH_GROUPS
+from LuciferFilter_Robot import ADMINS, AUTH_CHANNEL, LOG_CHANNEL, AUTH_USERS, AUTH_GROUPS, CUSTOM_FILE_CAPTION, P_TTI_SHOW_OFF, IMDB, SINGLE_BUTTON, SPELL_CHECK_REPLY, IMDB_TEMPLATE
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from pyrogram.handlers import CallbackQueryHandler
 from pyrogram import Client, filters, enums 
