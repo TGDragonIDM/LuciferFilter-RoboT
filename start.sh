a5834b5a5107db1440ba0917983ca79535eb9233
Bikash Maity <126913141+PR0FESS0R-TG@users.noreply.github.com>
2023-03-04 08:34:01 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b main https://github.com/Pr0fess0r99/LuciferFilter-RoboT.git /LuciferFilter-RoboT
+git clone -b main https://github.com/PR0FESS0R-TG/LuciferFilter-RoboT.git /LuciferFilter-RoboT
 cd /LuciferFilter-RoboT
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
